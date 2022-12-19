d3e96e0e38a449eb71c6e4a7922c0e96bc401dfc
Samip <ghimiresamip91@gmail.com>
2022-12-19 16:30:10 +0545

    initial commit

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -2,7 +2,7 @@ const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl:
         process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `https://papaya-rabanadas-9b725a.netlify.app/`
+            ? process.env.SITE_URL || config.siteUrl || `https://papaya-rabanadas-9b725a.netlify.app`
             : `http://localhost:8000`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
